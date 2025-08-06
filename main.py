ff258e076c2bb2e81872ea5617dc84121cb352de
CybinTalus <CybinTalus@proton.me>
2025-08-06 17:52:01 -0400

    Created player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 # Variables
 background_color = (0, 0, 0)
@@ -8,14 +9,32 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    
     dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, background_color)
-        pygame.display.flip()
+        
+        for item in updatable:
+            item.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+        
+        # player.draw(screen)
+        # player.update(dt)
+        pygame.display.flip() # (re)renders the screen
         
         dt = clock.tick(60) / 1000
 
